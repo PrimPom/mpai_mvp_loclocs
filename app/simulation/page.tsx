6890186fe1522677f0e2c61a80d7ae12491ea926
nabilWafick <nabilsoumanou7@gmail.com>
2025-05-29 17:35:48 +0100

    init push

--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function SimulaionPage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <span className="text-primary">TEST</span>
+      <Button variant={"outline"} className=" border-4">
+        SHADCN BUTTON
+      </Button>
     </div>
   );
 }
